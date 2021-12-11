bc294a5d8dee38710271efc5f585814cc0229a2a
AlluringManx23 <44430019+AlluringManx23@users.noreply.github.com>
2021-12-11 16:30:07 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 require('./app_api/models/db');
 
+const index = require('./app_server/routes/index');
 const apiRoutes = require('./app_api/routes/index');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -18,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
